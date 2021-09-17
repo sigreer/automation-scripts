5ebffebd9893fd0fc9a4a178753a54b40cfb2f66
root <root@web-dev1.merula.tilttech.local>
2021-09-17 01:40:24 +0100

    updated nginx compiler script

--- nginx-install.sh
+++ nginx-install.sh
@@ -85,5 +85,25 @@ systemctl stop apache2
 systemctl disable apache2
 mkdir /var/log/nginx
 mkdir /var/cache/nginx && chown www-data:www-data /var/cache/nginx
+mkdir /etc/nginx/sites-available && mkdir /etc/nginx/sites-enabled
+cat <<EOT >> 
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network-online.target remote-fs.target nss-lookup.target
+Wants=network-online.target
+
+[Service]
+Type=forking
+PIDFile=/run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t
+ExecStart=/usr/sbin/nginx
+ExecReload=/usr/sbin/nginx -s reload
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
 systemctl enable nginx
 systemctl start nginx
