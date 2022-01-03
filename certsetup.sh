14530f214a7b3d0fb4cad8d525653c05f34f6b15
sigreer <42386868+sigreer@users.noreply.github.com>
2022-01-03 21:12:36 +0000

    distro check

--- certsetup.sh
+++ certsetup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+source distrocheck.sh
+
+if [[ $DISTRO != *debian* ]]
+then
+  exit 0
+fi
+
 chmod 400 ./certificates/*
-cp /tilt/certificates/CAs/* /usr/local/share/ca-certificates/
+cp $tiltbasedir/certificates/CAs/* /usr/local/share/ca-certificates/
 update-ca-certificates
